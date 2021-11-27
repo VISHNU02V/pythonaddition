b0264622483b30522bf4b209adb26c49e9116f3d
Vishnu V <84262256+VISHNU02V@users.noreply.github.com>
2021-11-27 13:10:41 +0530

    Update pythonadditionusingfunc.py

--- pythonadditionusingfunc.py
+++ pythonadditionusingfunc.py
@@ -1,14 +1,18 @@
 # Addition using functions
 
-def add1(a,b):              # function to add
-    s=a+b
+
+def add1(a,b): # function to add
+    s=a+b                                                   # adding.....
     print("The sum of the 2 numbers,",a,"and",b,",is:",s)
+    
+    
 ch="y"
 while (ch=="y"):
     print("/////   TO ADD 2 NUMBERS   //////")
-    a=int(input("Please enter the 1st number::"))
-    b=int(input("Please enter the 2nd number::"))
+    a=int(input("Please enter the 1st number::"))           # inputing 1st number
+    b=int(input("Please enter the 2nd number::"))           # inputing 2nd number
     print("~~~~~~~ SUM OF 2 NUMBER ~~~~~~")
-    add1(a,b)
-    ch=input("Do you want to continue::")
+    add1(a,b)                                               # function call
+    ch=input("Do you want to continue::")                   # to control while loop
+
 print("///////THANK YOU///////")
